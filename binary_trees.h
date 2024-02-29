aa21952926c44d436eebeae1ba180e2b1929b605
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-02-28 16:41:17 -0800

    betty bugs

--- binary_trees.h
+++ binary_trees.h
@@ -13,12 +13,12 @@
  */
 typedef struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 } binary_tree_t;
 
-void binary_tree_print(const binary_tree_t *):
+void binary_tree_print(const binary_tree_t *);
 
 #endif /* _BINARY_TREES_H_ */
