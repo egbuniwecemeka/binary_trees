e61a48d3fdefd89d63d97b4c2a8d418f7ed47b2d
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-02-29 14:33:13 -0800

    Insert right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -6,7 +6,7 @@
  * @value:  value to store in the new node
  * Return:  a pointer to the created node, or NULL if parent is NULL
  *
- * Description: If parent already has a right-child, the new node must take 
+ * Description: If parent already has a right-child, the new node must take
  * its place, and the old right-child set as the right-child of the new node
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
